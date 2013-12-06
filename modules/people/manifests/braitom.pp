db66c46affb982bead00d9f8da0be67f86eaa085
braitom <braitone.tomtom@gmail.com>
2013-12-06 19:58:07 +0900

    bug fix

--- modules/people/manifests/braitom.pp
+++ modules/people/manifests/braitom.pp
@@ -17,8 +17,8 @@ class people::braitom {
   
   # OSX Settings
   include osx::dock::autohide
-  include osx::show_external_hard_drives_on_desktop
-  include osx::show_removale_media_on_desktop
+  include osx::finder::show_external_hard_drives_on_desktop
+  include osx::finder::show_removable_media_on_desktop
   include osx::show_hidden_files
   class { 'osx::dock::position':
     position => "left"
@@ -39,16 +39,16 @@ class people::braitom {
     'GoogleJapaneseInput':
       source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
       provider => pkgdmg;
-    'kobito':
-      source => "http://kobito.qiita.com/download/kobito_v1.8.4.zip"
-      provider => compressed_app;
-    'XtraFinder':
-      source => "http'//www.trankyunam.com/xtrafinder/downloads/XtraFinder.dmg"
-      provider => pkgdmg"
+    #'kobito':
+    #  source => "http://kobito.qiita.com/download/kobito_v1.8.4.zip",
+    #  provider => compressed_app;
+    #'XtraFinder':
+    #  source => "http://www.trankyunam.com/xtrafinder/downloads/XtraFinder.dmg",
+    #  provider => pkgdmg"
   }
 
   $home    = "/User/${::boxen_user}"
 
-  include python::2_7_5
-  class { 'python::global': version => '2.7.5' }
+  include python::2_7_3
+  class { 'python::global': version => '2.7.3' }
 }
